45fec66135312d153fab95d544fc8d139faafaef
Adam Schwalm <adamschwalm@gmail.com>
2013-11-12 08:14:30 -0600

    Added more missing tables

--- create_database.py
+++ create_database.py
@@ -12,7 +12,11 @@ db=_mysql.connect(host=host,user=user,
 
 setup = """
 
-DROP TABLE IF EXISTS project_experiments;
+DROP TABLE IF EXISTS used_on;
+DROP TABLE IF EXISTS use_experiments;
+DROP TABLE IF EXISTS use_equipment;
+DROP TABLE IF EXISTS use_reagents;
+DROP TABLE IF EXISTS use_specimens;
 DROP TABLE IF EXISTS measurements;
 DROP TABLE IF EXISTS projects;
 DROP TABLE IF EXISTS experiments;
@@ -28,16 +32,20 @@ CREATE TABLE projects(
     PRIMARY KEY (project_id)
 );
 
-CREATE TABLE experiments(
-    experiment_id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100),
-    PRIMARY KEY (experiment_id)
-);
-
 CREATE TABLE procedures(
     procedure_id INT NOT NULL AUTO_INCREMENT,
     description VARCHAR(1000),
     PRIMARY KEY (procedure_id)
+);
+
+CREATE TABLE experiments(
+    experiment_id INT NOT NULL AUTO_INCREMENT,
+    project_id INT NOT NULL,
+    procedure_id INT NOT NULL,
+    name VARCHAR(100),
+    PRIMARY KEY (experiment_id),
+    FOREIGN KEY (project_id) REFERENCES projects (project_id) ON DELETE CASCADE,
+    FOREIGN KEY (procedure_id) REFERENCES procedures (procedure_id) ON DELETE CASCADE
 );
 
 CREATE TABLE reagents(
@@ -48,6 +56,14 @@ CREATE TABLE reagents(
     PRIMARY KEY (reagent_id)
 );
 
+CREATE TABLE use_reagents(
+    procedure_id INT NOT NULL,
+    reagent_id INT NOT NULL,
+    PRIMARY KEY (reagent_id, procedure_id),
+    FOREIGN KEY (reagent_id) REFERENCES reagents (reagent_id) ON DELETE CASCADE,
+    FOREIGN KEY (procedure_id) REFERENCES procedures (procedure_id) ON DELETE CASCADE
+);
+
 CREATE TABLE equipment(
     equipment_id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100),
@@ -56,11 +72,27 @@ CREATE TABLE equipment(
     PRIMARY KEY (equipment_id)
 );
 
+CREATE TABLE use_equipment(
+    equipment_id INT NOT NULL,
+    procedure_id INT NOT NULL,
+    PRIMARY KEY (equipment_id, procedure_id),
+    FOREIGN KEY (equipment_id) REFERENCES equipment (equipment_id) ON DELETE CASCADE,
+    FOREIGN KEY (procedure_id) REFERENCES procedures (procedure_id) ON DELETE CASCADE
+);
+
 CREATE TABLE specimens(
     specimen_id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100),
     origin VARCHAR(100),
     PRIMARY KEY (specimen_id)
+);
+
+CREATE TABLE use_specimens(
+    specimen_id INT NOT NULL,
+    experiment_id INT NOT NULL,
+    PRIMARY KEY (specimen_id, experiment_id),
+    FOREIGN KEY (specimen_id) REFERENCES specimens (specimen_id) ON DELETE CASCADE,
+    FOREIGN KEY (experiment_id) REFERENCES experiment (experiment_id) ON DELETE CASCADE
 );
 
 CREATE TABLE measurements(
@@ -72,15 +104,13 @@ CREATE TABLE measurements(
     FOREIGN KEY (specimen_id) REFERENCES specimens (specimen_id) ON DELETE CASCADE
 );
 
-#Map a project to its experiments
-CREATE TABLE project_experiments(
-    project_id INT NOT NULL,
-    experiment_id INT NOT NULL,
-    PRIMARY KEY (project_id, experiment_id),
-    FOREIGN KEY (project_id) REFERENCES projects (project_id) ON DELETE CASCADE,
-    FOREIGN KEY (experiment_id) REFERENCES experiments (experiment_id) ON DELETE CASCADE
+CREATE TABLE used_on(
+    specimen_id INT NOT NULL,
+    procedure_id INT NOT NULL,
+    PRIMARY KEY (specimen_id, procedure_id),
+    FOREIGN KEY (specimen_id) REFERENCES specimens (specimen_id) ON DELETE CASCADE,
+    FOREIGN KEY (procedure_id) REFERENCES procedures (procedure_id) ON DELETE CASCADE
 );
-
 
 """
 
